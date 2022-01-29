88709be35bfec11f8f20f2e8dc3e72ed83e6b021
Aarón López <n_aaron5@hotmail.com>
2022-01-29 14:39:51 +0100

    Index page get popular movies with axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 import AppRoutes from './routes/AppRoutes'
-
+import SearchAppBar from './components/SearchAppBar/SearchAppBar'
 
 function App() {
   return (
     <div className="App">
-      
+      <SearchAppBar />
       <AppRoutes />
 
     </div>
--- client/src/pages/IndexPage/IndexPage.jsx
+++ client/src/pages/IndexPage/IndexPage.jsx
@@ -1,9 +1,24 @@
-import SearchAppBar from '../../components/SearchAppBar/SearchAppBar'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const IndexPage = () => {
 
+    let API_URL = `${process.env.REACT_APP_SERVER_URL}/popular/movies`
+    const [data, setData] = useState()
+
+    useEffect( () => {
+        const axiosCall = async () => {
+            const dataFromAxios = await axios.get(API_URL)
+            setData(dataFromAxios.data.popularMovies)
+        }
+        axiosCall()
+    }, [])
+
+    console.log(data)
     return (
-        <SearchAppBar />
+        data.map((movie) => {
+            return <p>{movie.title}</p>
+        })
     )
 }
 
